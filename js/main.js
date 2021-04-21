4efb787fe0b9a0ea8e875f6d03fc8db3ce986123
ezequiel <ezequiel.buzzella@gmail.com>
2021-04-21 19:20:52 -0300

    Intento de captcha y agregado estilo al nav

---
+++ js/main.js
@@ -0,0 +1,7 @@
+"uses strict";
+const value=5;
+let btn_formulario = document.getElementById("btn-enviar").addEventListener("click",EnviarFormulario);
+function EnviarFormulario(){
+    let resultado=document.getElementById("Captcha").value;
+    
+}
