1a5ddfe312b43c6d752fd04fee294d954a90faf6
ezequiel <ezequiel.buzzella@gmail.com>
2021-04-22 11:29:12 -0300

    Terminado captcha

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 const value=5;
 let btn_formulario = document.getElementById("btn-enviar").addEventListener("click",EnviarFormulario);
 function EnviarFormulario(){
-    let resultado=document.getElementById("Captcha").value;
-    
+    let Resultado=document.querySelector("#Captcha").value;
+    console.log(Resultado)
+    if (Resultado==value){
+        document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue envuada"
+    }else{
+        document.querySelector("#Resultado-Captcha").innerHTML= "Error vuelva a intentarlo"
+
+    }
+
 }
