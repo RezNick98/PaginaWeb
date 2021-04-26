2b7f615d470cd0350b98d53d40855ce3884b09a0
ezequiel <ezequiel.buzzella@gmail.com>
2021-04-26 09:33:35 -0300

    Agregado Especificaciones.html (donde iria la tabla) ademas agregado dos autos mas

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 "uses strict";
-const value=5;
+const valueCaptcha=5;
 let btn_formulario = document.getElementById("btn-enviar").addEventListener("click",EnviarFormulario);
 function EnviarFormulario(){
     let Resultado=document.querySelector("#Captcha").value;
-    console.log(Resultado)
-    if (Resultado==value){
-        document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue envuada"
+    if (Resultado==valueCaptcha){
+        document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue enviada"
     }else{
         document.querySelector("#Resultado-Captcha").innerHTML= "Error vuelva a intentarlo"
 
