b5964e936f031560f39df5d93014346e7b517107
ezequiel <ezequiel.buzzella@gmail.com>
2021-04-30 16:05:04 -0300

    Creado el captcha en random y arreglado algo de estilo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 "uses strict";
-const valueCaptcha=5;
 let btn_formulario = document.getElementById("btn-enviar").addEventListener("click",EnviarFormulario);
+let valueCaptcha=Math.floor((Math.random()*100)+1);
+document.querySelector("#numero").innerHTML=valueCaptcha;
 function EnviarFormulario(){
     let Resultado=document.querySelector("#Captcha").value;
     if (Resultado==valueCaptcha){
         document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue enviada"
     }else{
-        document.querySelector("#Resultado-Captcha").innerHTML= "Error vuelva a intentarlo"
-
+        document.querySelector("#Resultado-Captcha").innerHTML="Resultado incorrecto,vuelva a intentarlo";
     }
-
 }
