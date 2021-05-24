5dc44831f72eb8b2a4d9ecb4c0181fc8c217a6c8
ezequiel <ezequiel.buzzella@gmail.com>
2021-05-24 17:22:44 -0300

    sduihfidshufhui

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let btn_formulario = document.getElementById("btn-enviar").addEventListener("cli
 let valueCaptcha=Math.floor((Math.random()*100)+1);
 document.querySelector("#numero").innerHTML=valueCaptcha;
 function EnviarFormulario(){
+    event.preventDefault()
     let Resultado=document.querySelector("#Captcha").value;
     if (Resultado==valueCaptcha){
         document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue enviada"
