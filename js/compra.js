40543bf21bee7d230bab604437e0e94c4d9f1ddd
Maximiliano-Otharan <maxileonel.oth@gmail.com>
2021-05-30 00:16:58 -0300

    terminado pagina de compra

--- js/compra.js
+++ js/compra.js
@@ -1,161 +1,159 @@
 "use strict"
-let tabla = [
+
+////////Formulario que no envia////////////////////////////////////////////////////////////////////////////////////
+
+let form = document.querySelector("#form");
+form.addEventListener("submit", noEnvia);
+
+function noEnvia(e){
+    e.preventDefault(e);
+    console.log("funciona");
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+let tabla =[
     {
-        "Nombre": "Evolution VII",
-        "Precio": "$100.000",
+        "nombre": "Evolution VII",
+        "precio": "100000"
     },
     {
-        "Nombre": "Volkswagen Fox",
-        "Precio": "$20.000",
+        "nombre": "Evolution VII",
+        "precio": "100000"
     },
     {
-        "Nombre": "Chevrolet corsa",
-        "Precio": "$8.000",
-    }
+        "nombre": "Evolution VII",
+        "precio": "100000"
+    },
 ];
 
-let t = document.getElementById("tabla-dinamica");
-        t.innerHTML = " ";
-
-    for (const o of tabla) {
-    t.innerHTML += `<thead>
-            <th>Modelo</th>
-            <th>Precio</th>
-        </thead>
-        <tbody>
-            <td> ${o.Nombre} </td>
-            <td> ${o.Precio} </td>
-        </tbody>
-        `
-    }
-
-document.getElementById("btn-evo").addEventListener("click", compra1);
-document.getElementById("btn-supra").addEventListener("click", compra2);
-document.getElementById("btn-m30").addEventListener("click", compra3);
-document.getElementById("btn-corsa").addEventListener("click", compra4);
-document.getElementById("btn-fox").addEventListener("click", compra5);
-document.getElementById("btn-golf").addEventListener("click", compra6);
-
-
-function compra1() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-evo").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-evo").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-function compra2() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-supra").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-supra").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-function compra3() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-m3").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-m3").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-function compra4() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-corsa").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-corsa").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-function compra5() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-fox").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-fox").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-function compra6() {
-    event.preventDefault();
-    let nombre = document.getElementById("nombre-golf").innerHTML;
-        console.log(nombre);
-    let precio = document.getElementById("valor-golf").innerHTML;
-        console.log(precio);
-
-        nuevoAuto(nombre, precio);
-        actualizar();
-}
-
-function nuevoAuto(n, p) {
-    let nuevoAuto = {
-        "Nombre": n,
-        "Precio": p,
-    };
-    tabla.push(nuevoAuto);
-}
-
-function actualizar() {
-    event.preventDefault();
+function mostrar() {
     let t = document.getElementById("tabla-dinamica");
-        t.innerHTML = " ";
 
-    for (const o of tabla) {
-    t.innerHTML += `<thead>
+    t.innerHTML = " ";
+
+    for (const i of tabla) {
+        t.innerHTML += `<thead>
             <th>Modelo</th>
             <th>Precio</th>
         </thead>
         <tbody>
-            <td> ${o.Nombre} </td>
-            <td> ${o.Precio} </td>
+            <td> ${i.nombre} </td>
+            <td> ${i.precio} </td>
         </tbody>
         `
     }
 }
 
-document.getElementById("agregar-varios").addEventListener("click", agregarVarios);
+mostrar();
 
-function agregarVarios(params) {
-    event.preventDefault();
-    let tabla2 = [
-        {
-            "Nombre": "Evolution VII",
-            "Precio": "$100.000",
-        },
-        {
-            "Nombre": "Supra",
-            "Precio": "$150.000",
-        },
-        {
-            "Nombre": "M3-M30",
-            "Precio": "$80.000",
-        },
-    ];
+////////Boton de borrar carrito////////////////////////////////////////////////////////////////////////////////////
 
-    for (let i= 0; i < tabla2.length; i++) {
-        tabla.push(tabla2[i]);
+function total() {
+    let valor = 0;
+
+    for (let i = 0; i < tabla.length; i++) {
+        valor += Number(tabla[i].precio);
     }
-    actualizar();
+
+    console.log(valor);
+
+    document.getElementById("total").innerHTML = "$" + valor;
 }
 
-document.getElementById("borrar").addEventListener("click", borrar);
+total();
 
-function borrar() {
-    event.preventDefault();
-    for (let i= tabla.length; i > 0; i--) {
-        tabla.pop(i);
-    }
-    actualizar();
-}
+////////////////////////////////////////////////////////////////////////////////////
+
+////////Botones de comprar////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("btn-evo").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-evo").innerHTML;
+    let precio = document.getElementById("valor-evo").innerHTML;
+        comprar(nombre, precio);
+});
+
+document.getElementById("btn-supra").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-supra").innerHTML;
+    let precio = document.getElementById("valor-supra").innerHTML;
+        comprar(nombre, precio);
+});
+
+document.getElementById("btn-m30").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-m30").innerHTML;
+    let precio = document.getElementById("valor-m30").innerHTML;
+        comprar(nombre, precio);
+});
+
+document.getElementById("btn-corsa").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-corsa").innerHTML;
+    let precio = document.getElementById("valor-corsa").innerHTML; 
+        comprar(nombre, precio);
+});
+
+document.getElementById("btn-fox").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-fox").innerHTML;
+    let precio = document.getElementById("valor-fox").innerHTML
+        comprar(nombre, precio);
+});
+
+document.getElementById("btn-golf").addEventListener("click", function(e){
+    let nombre = document.getElementById("nombre-golf").innerHTML;
+    let precio = document.getElementById("valor-golf").innerHTML 
+        comprar(nombre, precio);
+});
+
+function comprar(n, p){
     
+    let nuevoAuto ={
+        "nombre": n,
+        "precio": p,
+    };
+
+    tabla.push(nuevoAuto);
+    mostrar();
+    total();
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+
+////////Boton de agregar premiums////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("agregar-varios").addEventListener("click", function(e){
+
+    let autosTop =[{
+        "nombre": "Evolution VII",
+        "precio": "100000",
+    },
+    {
+        "nombre": "M4 Supra",
+        "precio": "150000",
+    },
+    {
+        "nombre": "M3-m30",
+        "precio": "80000",
+    },
+];
+
+    for (let i = 0; i < autosTop.length; i++) {
+        tabla.push(autosTop[i]);
+    }
+
+    mostrar();
+    total();
+
+});
+
+////////////////////////////////////////////////////////////////////////////////////
+
+////////Boton de borrar carrito////////////////////////////////////////////////////////////////////////////////////
+
+document.getElementById("borrar").addEventListener("click", function(e){ borrar(tabla); mostrar(); total();});
+
+function borrar(t) {
+    for (let i = t.length; i > 0; i--) {
+        tabla.pop();
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////
--- js/contacto.js
+++ js/contacto.js
@@ -1,9 +1,14 @@
 "uses strict";
+let form = document.getElementById("form").addEventListener("submit", noEnviar);
+
+function noEnviar() {
+    event.preventDefault();
+}
+
 let btn_formulario = document.getElementById("btn-enviar").addEventListener("click",EnviarFormulario);
 let valueCaptcha=Math.floor((Math.random()*100)+1);
 document.querySelector("#numero").innerHTML=valueCaptcha;
 function EnviarFormulario(){
-    event.preventDefault()
     let Resultado=document.querySelector("#Captcha").value;
     if (Resultado==valueCaptcha){
         document.querySelector("#Resultado-Captcha").innerHTML="Resultado correcto, su consulta fue enviada"
