5b95101faacdf827b1c3d0760ad52df15e8b7e6f
Maximiliano-Otharan <maxileonel.oth@gmail.com>
2021-06-30 00:27:43 -0300

    tuve que re empezar y pensarlo de nuevo

--- js/compra.js
+++ js/compra.js
@@ -12,13 +12,6 @@ function noEnvia(e){
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-/*La tabla inicia precargada con un inicio de compra == 0 */
-
-let inicioTabla = false;
-
-let tabla =[];
-
-let datosAutos =[];
 ////////Fetch////////////////////////////////////////////////////////////////////////////////////
 const url = "https://60c6d7da19aa1e001769fcab.mockapi.io/api/Autos";
 async function obtenerDatos() {
@@ -28,7 +21,6 @@ async function obtenerDatos() {
                 console.log(json);
         for(const autos of json){
             tabla.push(autos);
-            datosAutos.push(autos);
         }
     }catch(error){
         console.log(error);
@@ -40,7 +32,36 @@ async function obtenerDatos() {
 
 obtenerDatos();
 
+let id = 0;
+
 ////////////////////////////////////////////////////////////////////////////////////
+
+async function BorrarApi(id) {
+
+    try{
+        let res = await fetch(`${url}/${id}`, {
+            "method": "DELETE"
+        });
+            if(res.status === 200){
+                console.log("Borrado carrito");
+            }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+/*La tabla inicia precargada con un inicio de compra == 0 */
+
+let inicioTabla = false;
+
+// function inicio() {
+//     if(inicioTabla == false){
+//         inicioTabla = true;
+//             borrar(tabla);
+//     }
+// }
+
+let tabla =[];
 
 function mostrar() {
     let t = document.getElementById("tabla-dinamica");
@@ -48,6 +69,8 @@ function mostrar() {
     t.innerHTML = " ";
 
     let style = 0;
+    let idMod = 0;
+    let idEliminar = 0;
 
     for (const i of tabla) {
 
@@ -59,10 +82,14 @@ function mostrar() {
             <td> ${i.nombre} </td>
             <td id="${style}"> ${i.precio} </td>
             <td> ${i.contador} </td>
+            <td> <button id="mod${idMod}">Modificar</button> </td>
+            <td> <button id="eliminar${idEliminar}">Eliminar</button> </td>
         </tbody>
         `
         colorear(style);
         style++;
+        idMod++;
+        idEliminar++;
     }
     
 }
@@ -97,82 +124,79 @@ function total() {
 
 total();
 
-function inicio() {
-    if(inicioTabla == false){
-        inicioTabla = true;
-            borrar(tabla);
-    }
-}
-
-async function agregoApi(a) {
-
-    let agregoAuto = a;
-
-    try{
-        let res = await fetch(url, {
-            "method": "POST",
-            "headers": {"Content-type": "application/json"},
-            "body": JSON.stringify(agregoAuto)
-        });
-            if(res.status === 201){
-                console.log("Agregado con exito");
-            }
-    }catch(error){
-        console.log(error);
-    }
-}
-
 ////////////////////////////////////////////////////////////////////////////////////
 
-let UlimoAutoComprado = {};
+let UlimoAutoComprado = [];
 
 ////////Botones de comprar////////////////////////////////////////////////////////////////////////////////////
+let contadorA = 0;
 document.getElementById("btn-evo").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-evo").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-evo").innerHTML;
+        contadorA++;
+            id++;
+        comprar(nombre, precio, contadorA);
 });
 
+    let contadorB = 0;
 document.getElementById("btn-supra").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-supra").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-supra").innerHTML;
+        contadorB++;
+            id++;
+        comprar(nombre, precio, contadorB);
 });
 
+    let contadorC = 0
 document.getElementById("btn-m30").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-m30").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-m30").innerHTML;
+        contadorC++;
+            id++;
+        comprar(nombre, precio, contadorC);
 });
 
+    let contadorD = 0
 document.getElementById("btn-corsa").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-corsa").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-corsa").innerHTML;
+        contadorD++;
+            id++;
+        comprar(nombre, precio, contadorD);
 });
 
+    let contadorE = 0
 document.getElementById("btn-fox").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-fox").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-fox").innerHTML;
+        contadorE++;
+            id++;
+        comprar(nombre, precio, contadorE);
 });
 
+    let contadorF = 0;
 document.getElementById("btn-golf").addEventListener("click", function(e){
     let nombre = document.getElementById("nombre-golf").innerHTML;
-        comprar(nombre);
+    let precio = document.getElementById("valor-golf").innerHTML;
+        contadorF++;
+            id++;
+        comprar(nombre, precio, contadorF);
 });
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-function comprar(n) {
+function comprar(n,p,c) {
 
-    inicio();
+    // inicio();
 
-    let nuevoAuto ={};
+    let nuevoAuto ={
+        "nombre": n,
+        "precio": Number(p),
+        "contador": Number(c),
+        "id": id,
+    };
 
-    for (const auto of datosAutos) {
-        if(n == auto.nombre){
-                nuevoAuto = auto;
-                console.log(nuevoAuto);
-        }
-    }
-
-    UlimoAutoComprado = nuevoAuto;
+    UlimoAutoComprado.push(nuevoAuto);
 
     Top(nuevoAuto);
 
@@ -182,6 +206,90 @@ function comprar(n) {
 
 ////////Busco iguales y si no encuentro agrego a la tabla////////////////////////////////////////////////////////////////////////////////////
 
+async function agregarApi(a){
+    let auto = a;
+
+    try{
+        let res = await fetch(url, {
+            "method": "POST",
+            "headers": {"Content-type": "application/json"},
+            "body": JSON.stringify(auto)
+        })
+            if(res.status === 201){
+                console.log("Agregado al carrito");
+            }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+async function modificarApi(a) {
+
+    try{
+        let res = await fetch(`${url}/${a.id}`, {
+            "method": "PUT",
+            "headers": {"Content-type": "application/json"},
+            "body": JSON.stringify(a)
+        });
+        if(res.status === 200){
+            console.log("modificado");
+        }
+    }catch(error){
+        console.log(error);
+    }
+}
+
+document.getElementById("agregar-varios").addEventListener("click", function(e) {
+    id++;
+        contadorA++;
+            contadorB++;
+                contadorC++;
+        agregarPremiums(id, contadorA, contadorB,contadorC);
+});
+
+function agregarPremiums(id, contadorA, contadorB,contadorC) {
+    
+    let premiumsA = {
+        "nombre": "Evolution VII",
+        "precio": 100000,
+        "contador": contadorA,
+        "id": id
+    };
+
+    id++;
+
+    UlimoAutoComprado.push(premiumsA);
+
+    Top(premiumsA);
+
+
+    let premiumsB = {
+        "nombre": "M4 Supra",
+        "precio": 150000,
+        "contador": contadorB,
+        "id": id,
+    };
+
+    id++
+
+    UlimoAutoComprado.push(premiumsB);
+
+    Top(premiumsB);
+
+    let premiumsC = {
+        "nombre": "M3-m30",
+        "precio": 80000,
+        "contador": contadorC,
+        "id": id
+    };
+
+    UlimoAutoComprado.push(premiumsC);
+
+    Top(premiumsC);
+
+    
+}
+
 function Top(a) {
      let x = false;
 
@@ -189,58 +297,19 @@ function Top(a) {
          if(a.nombre == tabla[i].nombre){
              x = true;
                  tabla[i].contador++;
-                 datosAutos[i].contador = Number(datosAutos.contador++);
-                 modificarApi(a);
+                 let auto = tabla[i];
+                    agregarApi(auto);
          }
      }
 
      if(x == false){
-         tabla.push(a);
+        agregarApi(a);
+            tabla.push(a);
      }
 }
 
-async function modificarApi(a){
-
-    let id = a.id;
-
-    let auto = a;
-
-    try{
-        let res = await fetch(`${url}/${id}`,{
-            "method": "PUT",
-            "headers": {"Content-type": "application/json"},
-            "body": JSON.stringify(auto),
-        })
-            if(res.status === 200){
-                console.log("Modificado");
-            }
-    }catch(error){
-        console.log(error);
-    }
-}
 
 ////////////////////////////////////////////////////////////////////////////////////
-
-document.getElementById("editarDOM").addEventListener("click", Editar);
-
-function Editar() {
-
-        let numero = document.getElementById("modId").value;
-        let cantidad = document.getElementById("modCantidad").value;
-
-        for (let i = 0; i < tabla.length; i++) {
-            if(numero == tabla[i].id){
-                tabla[i].contador = cantidad;
-                datosAutos[i].contador = Number(cantidad);
-                modificarApi(datosAutos[i]);
-            }
-        }
-
-        mostrar();
-
-    document.getElementById("modId").value = " ";
-    document.getElementById("modCantidad").value = " ";
-}
 
 ////////Boton de borrar carrito////////////////////////////////////////////////////////////////////////////////////
 
@@ -253,52 +322,44 @@ function borrar(t) {
     for (let i = t.length; i > 0; i--) {
         tabla.pop();
     }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
 
-async function borrarUltimoApi(a){
-
-    let id = a.id;
-
-    try{
-        let res = await fetch(`${url}/${id}`, {
-            "method": "DELETE"
-        });
-            if(res.status === 200){
-                console.log("Eliminado con exito");
-            }
-    }catch(error){
-        console.log(error);
-    }
-}
-
 ////////Boton de borrar utlimo////////////////////////////////////////////////////////////////////////////////////
 
 document.getElementById("borrar-ultimo").addEventListener("click", function(e) {
-    let ultimo = UlimoAutoComprado;
+    let ultimo = [];
+    ultimo = UlimoAutoComprado;
     borrarUltimo(ultimo);
-    UlimoAutoComprado = {};
+    ultimo = [];
 });
 
 function borrarUltimo(u) {
 
-    let ultimo = u;
-        console.log(ultimo);
+    let ultimo = [];
+        ultimo = u;
+
+    let uComprado = {};
+    let pos = ultimo.length - 1;
+        uComprado = ultimo[pos];
+
+    let id = uComprado.id;
 
         for (let  i = 0; i < tabla.length; i++) {
-            if(tabla[i].nombre == ultimo.nombre){
+            if(tabla[i].nombre == uComprado.nombre){
                 if(tabla[i].contador == 1){
                     console.log("hay uno");
+                    BorrarApi(id);
                         tabla.splice(i, 1);
-                            datosAutos[i].contador--;
-                                borrarUltimoApi(ultimo);
+                            UlimoAutoComprado.splice(pos, 1);
                 }else{
                     if(tabla[i].contador > 1){
                         console.log("hay mas");
                             tabla[i].contador--;
-                                datosAutos[i].contador--;
-                                    modificarApi(tabla[i]);
+                                BorrarApi(id);
+                                    UlimoAutoComprado.splice(pos, 1)
                     }
                 }
             }
